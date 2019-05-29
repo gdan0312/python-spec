c1658deed3d2348a24b2fce741c9229e94c3b0bb
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2019-05-29 19:06:15 +0700

    Добавлен пример реализации собственного итератора

---
+++ course_01/week_04/square_iterator.py
@@ -0,0 +1,27 @@
+"""
+Пример реализации собственного итератора
+"""
+
+
+class SquareIterator:
+    """
+    Итератор возвращает квадраты чисел в заданном диапазоне
+    """
+    def __init__(self, start, end):
+        self.current = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        result = self.current ** 2
+        self.current += 1
+        return result
+
+
+if __name__ == '__main__':
+    for num in SquareIterator(1, 4):
+        print(num)
