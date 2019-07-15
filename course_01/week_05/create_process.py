0e0d6fbbf54f185185955b18013d09af84a88939
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2019-07-15 19:23:08 +0700

    Добалены примеры создания процесса и сервера

---
+++ course_01/week_05/create_process.py
@@ -0,0 +1,14 @@
+# создание процесса на Python
+import os
+import time
+
+pid = os.fork()
+if pid == 0:
+    # дочерний процесс
+    while True:
+        print('child:', os.getpid())
+        time.sleep(5)
+else:
+    # родительский процесс
+    print('parent:', os.getpid())
+    os.wait()
