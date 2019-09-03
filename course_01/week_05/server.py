895eb59b1d3cc919d583576fefe191171edb21ea
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2019-09-03 17:28:12 +0700

    Добавены заготовки для клиента и сервера финального проекта

---
+++ course_01/week_05/server.py
@@ -0,0 +1,32 @@
+import asyncio
+
+
+class ClientServerProtocol(asyncio.Protocol):
+    metrics = {}
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        resp = self.process_data(data.decode())
+        self.transport.write(resp.encode())
+
+    def process_data(self, data):
+        pass
+
+
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(ClientServerProtocol, host, port)
+    server = loop.run_until_complete(coro)
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+
+if __name__ == '__main__':
+    run_server('127.0.0.1', 8888)
