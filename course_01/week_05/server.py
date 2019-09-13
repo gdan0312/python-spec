5b444133a635b823b416830fbf180fa2ed3f46c8
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2019-09-13 20:33:02 +0700

    Продолжение работы над сервером для отправки метрик

--- course_01/week_05/server.py
+++ course_01/week_05/server.py
@@ -12,7 +12,29 @@ class ClientServerProtocol(asyncio.Protocol):
         self.transport.write(resp.encode())
 
     def process_data(self, data):
-        pass
+        if data.startswith('put'):
+            _, name, value, timestamp = data.split(' ')
+            if name not in ClientServerProtocol.metrics:
+                ClientServerProtocol.metrics[name] = [(timestamp, value)]
+            else:
+                ClientServerProtocol.metrics[name].append((timestamp, value))
+            return 'ok\n\n'
+        elif data.startswith('get'):
+            _, name = data.split(' ')
+            return self.get_metrics(name)
+        else:
+            return 'error\nwrong command\n\n'
+
+    def get_metrics(self, key):
+        if key == '*':
+            return 'ok\n\n'
+        if key not in ClientServerProtocol.metrics:
+            return 'ok\n\n'
+        resp = 'ok'
+        for tuple_ in ClientServerProtocol.metrics[key]:
+            resp += '\n{key} {value} {timestamp}'.format(key=key, value=tuple_[1], timestamp=tuple_[0])
+        resp += '\n\n'
+        return resp
 
 
 def run_server(host, port):
