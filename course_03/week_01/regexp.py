a4469356d2faeb24323c29d35bd665dc38e66465
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2020-05-03 17:45:55 +0700

    Обновлено решение задания по регулярным выражениям

--- course_03/week_01/regexp.py
+++ course_03/week_01/regexp.py
@@ -4,10 +4,11 @@
 def calculate(data, findall):
     matches = findall(r'([a-c])([+-]?)=([a-c]?)([+-]?\d{,3})')
     for v1, sign, v2, n in matches:
+        right = data.get(v2, 0) + int(n or 0)
         if sign == '-':
-            data[v1] = data[v1] - data.get(v2, 0) - int(n or 0)
+            data[v1] -= right
         elif sign == '+':
-            data[v1] = data[v1] + data.get(v2, 0) + int(n or 0)
+            data[v1] += right
         else:
-            data[v1] = data.get(v2, 0) + int(n or 0)
+            data[v1] = right
     return data
