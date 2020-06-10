c3c7a7d2da678822d913b7c51e955cd8b88bf2f4
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2020-06-10 21:12:10 +0700

    Добавлены примеры по валидации данных

--- course_03/django_tutorials/formdummy/urls.py
+++ course_03/django_tutorials/formdummy/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^review/$', views.FormDummyView.as_view())
+    url(r'^review/$', views.SchemaView.as_view())
 ]
--- course_03/django_tutorials/formdummy/views.py
+++ course_03/django_tutorials/formdummy/views.py
@@ -1,18 +1,49 @@
-from django.shortcuts import render
+import json
+
 from django.views import View
+from django.shortcuts import render
+from django.http import JsonResponse
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from marshmallow.exceptions import ValidationError as MarshmallowError
+
+from .forms import DummyForm
+from .schemas import REVIEW_SCHEMA, ReviewSchema
 
 
 class FormDummyView(View):
     def get(self, request):
-        return render(request, 'form.html')
+        form = DummyForm()
+        return render(request, 'form.html', context={'form': form})
 
     def post(self, request):
-        text = request.POST.get('text')
-        grade = request.POST.get('grade')
-        image = request.FILES.get('image')
-        content = image.read()
-        return render(request, 'form.html', context={
-            'text': text,
-            'grade': grade,
-            'content': content
-        })
+        form = DummyForm(request.POST)
+        if form.is_valid():
+            context = form.cleaned_data
+            return render(request, 'form.html', context=context)
+        return render(request, 'error.html', context={'errors': form.errors})
+
+
+class SchemaView(View):
+    def post(self, request):
+        try:
+            document = json.loads(request.body)
+            validate(document, REVIEW_SCHEMA)
+            return JsonResponse(document, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({'errors': 'Invalid JSON'}, status=400)
+        except ValidationError as err:
+            return JsonResponse({'errors': err.message}, status=400)
+
+
+class MarshView(View):
+    def post(self, request):
+        try:
+            document = json.loads(request.body)
+            schema = ReviewSchema(strict=True)
+            data = schema.load(document)
+            return JsonResponse(data.data, status=201)
+        except json.JSONDecodeError:
+            return JsonResponse({'errors': 'Invalid JSON'}, status=400)
+        except MarshmallowError as err:
+            return JsonResponse({'errors': err.messages}, status=400)
