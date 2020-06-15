3c8806de062d1437be300b92b39f6b4ab35b92c3
Горбунов Даниил Николаевич <d.gorbunov@2gis.ru>
2020-06-15 08:32:06 +0700

    Удалено приложение feedback

--- course_03/django_tutorials/feedback/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('add', views.FeedbackCreateView.as_view(), name='feedback-create'),
-    path('', views.FeedbackCreateView.as_view(), name='feedback-list')
-]
